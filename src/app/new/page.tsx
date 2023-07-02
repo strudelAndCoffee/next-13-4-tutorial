3225c061f8210e1d47b2173c18308cd1713e028b
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2023-07-02 15:03:08 -0500

    add create todo function

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,11 +1,31 @@
+import { prisma } from '@/db'
+import { redirect } from 'next/navigation'
+import Link from 'next/link'
+
+async function createTodo(data: FormData) {
+  'use server'
+
+  const title = data.get('title')?.valueOf()
+  if (typeof title !== 'string' || title.length === 0) {
+    throw new Error('invalid Title')
+  }
+
+  await prisma.todo.create({ data: { title, complete: false } })
+  redirect('/')
+}
+
 export default function Page() {
   return (
     <>
       <header>
         <h1>New</h1>
       </header>
-      <form>
+      <form action={createTodo}>
         <input type="text" name="title" />
+        <div>
+          <Link href="..">Cancel</Link>
+          <button type="submit">Create</button>
+        </div>
       </form>
     </>
   )
