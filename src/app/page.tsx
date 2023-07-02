9a3beaa2e4e7071b7edbc9a965c528580e6e6ec4
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2023-07-02 15:09:45 -0500

    add toggle function to todos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,12 @@ function getTodos() {
   return prisma.todo.findMany()
 }
 
+async function toggleTodo(id: string, complete: boolean) {
+  'use server'
+
+  await prisma.todo.update({ where: { id }, data: { complete } })
+}
+
 export default async function Home() {
   const todos = await getTodos()
 
@@ -17,7 +23,7 @@ export default async function Home() {
       </header>
       <ul>
         {todos.map((todo) => (
-          <TodoItem key={todo.id} {...todo} />
+          <TodoItem key={todo.id} {...todo} toggleTodo={toggleTodo} />
         ))}
       </ul>
     </>
--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,13 +1,26 @@
+'use client'
+
 type TodoItemProps = {
   id: string
   title: string
   complete: boolean
+  toggleTodo: (id: string, complete: boolean) => void
 }
 
-export default function TodoItem({ id, title, complete }: TodoItemProps) {
+export default function TodoItem({
+  id,
+  title,
+  complete,
+  toggleTodo,
+}: TodoItemProps) {
   return (
     <li>
-      <input id={id} type="checkbox" />
+      <input
+        id={id}
+        type="checkbox"
+        defaultChecked={complete}
+        onChange={(e) => toggleTodo(id, e.target.checked)}
+      />
       <label htmlFor={id}>{title}</label>
     </li>
   )
